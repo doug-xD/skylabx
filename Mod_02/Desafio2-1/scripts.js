a88d8f221cb187573e55de183d57a9db70213b38
doug_xD <growth.doug@gmail.com>
2020-06-28 10:47:58 -0300

    Final do Desafio. Alguns ajustes de ordenação/alinhamentos ainda precisam ser refeitos.

--- Mod_02/Desafio2-1/scripts.js
+++ Mod_02/Desafio2-1/scripts.js
@@ -5,7 +5,8 @@ for (let course of courses) {
     course.addEventListener("click", function(){
         const course_id = course.getAttribute("id")
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src = `http://www.globo.com`
+        modalOverlay.classList.toggle("maximized")
+        modalOverlay.querySelector("iframe").src = `https://www.rocketseat.com.br/${course_id}`
     })
 }
 
@@ -14,3 +15,6 @@ document.querySelector(".close_modal").addEventListener("click", function(){
     modalOverlay.querySelector("iframe").src = ""
 })
 
+document.querySelector(".max_modal").addEventListener("click", function(){
+    modalOverlay.classList.toggle("maximized")
+})
